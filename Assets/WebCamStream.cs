2c4b8f1a12470fb838db40beccdd103d6e6d3406
Martin Eklund <music@teadrinker.net>
2020-08-31 04:12:06 +0200

    web cam test

---
+++ Assets/WebCamStream.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WebCamStream : MonoBehaviour
+{
+    private WebCamTexture webcamTexture;
+
+    void OnEnable()
+    {
+        webcamTexture = new WebCamTexture();
+        Renderer renderer = GetComponent<Renderer>();
+        renderer.material.mainTexture = webcamTexture;
+        webcamTexture.Play();
+    }
+
+    void OnDisable()
+    {
+        if(webcamTexture != null)
+		{
+			webcamTexture.Stop();
+            // no dispose??
+        }
+    }
+
+
+}
